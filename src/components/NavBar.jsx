e99bcffbff43040bd0696a3c21787ebf04ecfe4e
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-07-27 11:10:11 -0400

    configure logout handler

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,12 +12,19 @@ import {
   MenuDivider,
 } from "@chakra-ui/react";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const [search, setSearch] = useState();
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState(false);
+
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   return (
     <div>
@@ -42,6 +49,14 @@ const NavBar = () => {
               <BellIcon />
             </MenuButton>
             <MenuList></MenuList>
+            <Menu>
+              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                My Profile
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
           </Menu>
         </div>
       </Box>
