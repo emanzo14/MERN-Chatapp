e57bf679593e6568c0f133281d431f2e843ac5e6
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-07-27 18:50:47 -0400

    finished create/access chats

--- src/components/MyChats.jsx
+++ src/components/MyChats.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { useToast } from "@chakra-ui/react";
+import { Box, useToast, Button, Stack, Text } from "@chakra-ui/react";
 import axios from "axios";
 
 const MyChats = ({ user }) => {
@@ -19,7 +19,7 @@ const MyChats = ({ user }) => {
       };
 
       const { data } = await axios.get("/api/chats", config);
-
+      console.log(data);
       setChats(data);
     } catch (error) {
       toast({
@@ -37,6 +37,63 @@ const MyChats = ({ user }) => {
     fetchChats();
   }, []);
 
-  return <div>My Chats</div>;
+  return (
+    <Box
+      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="pink"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        display="flex"
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+        <Button d="flex" fontSize={{ base: "17px", md: "10px", lg: "17px" }}>
+          New Group Chat
+        </Button>
+      </Box>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        width="100%"
+        height="95%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        <Stack overflowY="scroll">
+          {chats.map((chat) => (
+            <Box
+              onClick={() => setSelectedChat(chat)}
+              cursor="pointer"
+              bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+              color={selectedChat === chat ? "white" : "black"}
+              px={3}
+              py={2}
+              borderRadius="lg"
+              key={chat._id}
+            >
+              <Text>
+                {!chat.isGroupChat
+                  ? chat.users.map((user) => user.name).join(", ")
+                  : chat.chatName}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
+  );
 };
 export default MyChats;
