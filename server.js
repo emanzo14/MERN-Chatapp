aef2b604dd734d495b5bd4ff27837dd5e985a7b8
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-08-01 16:37:42 -0400

    heroku fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, function () {
   console.log(`Server.js is running on port ${PORT}`);
